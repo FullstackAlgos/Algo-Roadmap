e9062b3c9ba768a0abd31af7b660da9852589459
jasenvchan <jachan@gwu.edu>
2020-03-03 15:49:34 -0500

    prelim routing, no associations

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
+module.exports = router;
+
+router.use('/users', require('./users'));
+router.use('/problems', require('./problems'));
+router.use('/tags', require('./tags'));
+
 
 router.use((req, res, next) => {
   const err = new Error("API route not found!");
   err.status = 404;
   next(err);
 });
-
-module.exports = router;
