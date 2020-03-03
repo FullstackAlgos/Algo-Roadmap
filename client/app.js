2d25bf5c70af370c49c85f9c15c47047228201f3
James Shen <jjss886@gmail.com>
2020-03-03 16:17:14 -0500

    form initialiization

--- client/app.js
+++ client/app.js
@@ -5,15 +5,26 @@ import ProblemList from "./components/ProblemList";
 import QuestionForm from "./components/QuestionForm";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showForm: false
+    };
+  }
+
+  showFormFlip = () => {
+    this.setState({ showForm: !this.state.showForm });
+  };
+
   render() {
     return (
-      <div>
+      <div className="fullAppDiv">
         <div className="navBarDiv">
           <h4>App</h4>
         </div>
 
         <div className="belowNavDiv">
-          <User />
+          <User formFlip={this.showFormFlip} />
 
           <div className="rightContentDiv">
             <Roadmap />
@@ -22,7 +33,7 @@ class App extends Component {
           </div>
         </div>
 
-        <QuestionForm />
+        {this.state.showForm ? <QuestionForm /> : null}
       </div>
     );
   }
--- client/components/QuestionForm.js
+++ client/components/QuestionForm.js
@@ -1,8 +1,74 @@
 import React, { Component } from "react";
 
 class QuestionForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      description: "",
+      url: "",
+      image: ""
+    };
+  }
+
+  handleChange = evt => {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  };
+
+  handleNewQuest = evt => {
+    evt.preventDefault();
+    console.log("hello");
+  };
+
   render() {
-    return <div>Form</div>;
+    return (
+      <div className="questFormFullDiv">
+        Form
+        <form className="questForm" onSubmit={this.handleNewQuest}>
+          <label htmlFor="name">Question Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+            className="inputBox"
+          />
+
+          <label htmlFor="description">Description:</label>
+          <input
+            type="text"
+            name="description"
+            value={this.state.description}
+            onChange={this.handleChange}
+            className="inputBox"
+          />
+
+          <label htmlFor="url">Link:</label>
+          <input
+            type="text"
+            name="url"
+            value={this.state.url}
+            onChange={this.handleChange}
+            className="inputBox"
+          />
+
+          <label htmlFor="image">Image:</label>
+          <input
+            type="text"
+            name="image"
+            value={this.state.image}
+            onChange={this.handleChange}
+            className="inputBox"
+          />
+
+          <button type="submit" className="questSubmitBtn">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
