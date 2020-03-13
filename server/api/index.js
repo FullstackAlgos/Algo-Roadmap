b97b60e54b77cb9150af5e4fb0efecdfe537a6ea
James Shen <jjss886@gmail.com>
2020-03-13 18:16:10 -0400

    updating user login and signup functionality for both front and backend

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
 module.exports = router;
 
-router.use('/users', require('./users'));
-router.use('/problems', require('./problems'));
-router.use('/tags', require('./tags'));
-
+router.use("/users", require("./users"));
+router.use("/problems", require("./problems"));
+router.use("/tags", require("./tags"));
 
 router.use((req, res, next) => {
   const err = new Error("API route not found!");
