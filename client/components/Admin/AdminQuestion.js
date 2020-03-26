092b7d3291ec37f7ad49908bd90f4b9114bc7ea5
James Shen <jjss886@gmail.com>
2020-03-26 16:16:35 -0400

    showing edit form for each of the questions

--- client/components/Admin/AdminQuestion.js
+++ client/components/Admin/AdminQuestion.js
@@ -1,12 +1,73 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
-const AdminQuestion = ({ q }) => {
-  return (
-    <div className="adminQuestionDiv">
-      <h3>{q.name}</h3>
-    </div>
-  );
-};
+class AdminQuestion extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showEdit: false,
+      name: "",
+      description: ""
+    };
+  }
+
+  showEdit = () => {
+    this.setState({ showEdit: true });
+  };
+
+  handleChange = evt => {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  };
+
+  render() {
+    const { q } = this.props;
+
+    return (
+      <div className="adminQuestionDiv">
+        <div className="adminQuestRow1">
+          <h3 className="adminQuestName">{q.name}</h3>
+
+          <button type="button" onClick={this.showEdit}>
+            Edit
+          </button>
+        </div>
+
+        <p className="adminQuestDesc">{q.description}</p>
+
+        {this.state.showEdit ? (
+          <form className="adminQuestForm">
+            <div className="adminQuestFormDiv">
+              <label htmlFor="name" className="questLabels">
+                Question Name:
+              </label>
+              <input
+                type="text"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleChange}
+                className="inputBox"
+              />
+            </div>
+
+            <div className="adminQuestFormDiv">
+              <label htmlFor="description" className="questLabels">
+                Description:
+              </label>
+              <input
+                type="text"
+                name="description"
+                value={this.state.description}
+                onChange={this.handleChange}
+                className="inputBox"
+              />
+            </div>
+          </form>
+        ) : null}
+      </div>
+    );
+  }
+}
 
 export default connect()(AdminQuestion);
