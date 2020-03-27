45cbd16cfae6f1ce08a3bf788db3ed8f1804a15b
James Shen <jjss886@gmail.com>
2020-03-27 13:31:22 -0400

    laid out foundation and UI for admin user page

--- client/components/Admin/AdminUser.js
+++ client/components/Admin/AdminUser.js
@@ -2,14 +2,56 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class AdminUser extends Component {
+  makeAdmin = () => {
+    console.log("ADMIN");
+  };
+
+  removeAdmin = () => {
+    console.log("REMOVE ADMIN");
+  };
+
+  removeUser = () => {
+    console.log("REMOVE");
+  };
+
   render() {
     const { u } = this.props;
 
     return (
-      <div className="adminQuestionDiv">
+      <div className="adminUserDiv">
         <div className="adminQuestRow1">
           <h3 className="adminQuestName">{u.name}</h3>
+
+          {u.isAdmin ? (
+            <button
+              type="button"
+              onClick={this.removeAdmin}
+              className="adminQuestBtn gBtn"
+            >
+              Remove Admin
+            </button>
+          ) : (
+            <button
+              type="button"
+              onClick={this.makeAdmin}
+              className="adminQuestBtn gBtn"
+            >
+              Grant Admin
+            </button>
+          )}
+
+          <button
+            type="button"
+            onClick={this.removeUser}
+            className="adminQuestBtn gBtn"
+          >
+            Remove User
+          </button>
         </div>
+
+        <p className="adminUserText">
+          Completed Questions: {u.questions.length}
+        </p>
       </div>
     );
   }
