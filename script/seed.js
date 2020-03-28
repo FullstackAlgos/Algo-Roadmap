e0c0693943db577f7267caa2a69d8618d3ed61be
James Shen <jjss886@gmail.com>
2020-03-28 16:59:12 -0400

    added min cost climbing stairs

--- script/seed.js
+++ script/seed.js
@@ -352,5 +352,13 @@ const questionBulk2 = [
     difficulty: 1,
     link: "https://leetcode.com/problems/house-robber/",
     tagId: 11
+  },
+  {
+    name: "Min Cost Climbing Stairs",
+    description:
+      "On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed). Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the floor, and you can either start from the step with index 0, or the step with index 1.",
+    difficulty: 1,
+    link: "https://leetcode.com/problems/min-cost-climbing-stairs/",
+    tagId: 11
   }
 ];
