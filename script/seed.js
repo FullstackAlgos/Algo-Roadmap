bef0c60e5154c0008766166fb7740dcd75c890a2
James Shen <jjss886@gmail.com>
2020-04-03 15:32:34 -0400

    completed medium tree problems

--- script/seed.js
+++ script/seed.js
@@ -438,6 +438,31 @@ const questionBulk = [
     link: "https://leetcode.com/problems/path-sum-iii/",
     tagId: 5
   },
+  {
+    name: "Validate Binary Search Tree",
+    description:
+      "Given a binary tree, determine if it is a valid binary search tree (BST).",
+    difficulty: 3,
+    link: "https://leetcode.com/problems/validate-binary-search-tree/",
+    tagId: 5
+  },
+  {
+    name: "Delete Leaves With a Given Value",
+    description:
+      "Given a binary tree root and an integer target, delete all the leaf nodes with value target.",
+    difficulty: 3,
+    link: "https://leetcode.com/problems/delete-leaves-with-a-given-value/",
+    tagId: 5
+  },
+  {
+    name: "Sum of Nodes with Even-Valued Grandparent",
+    description:
+      "Given a binary tree, return the sum of values of nodes with even-valued grandparent.  (A grandparent of a node is the parent of its parent, if it exists.) If there are no nodes with an even-valued grandparent, return 0.",
+    difficulty: 3,
+    link:
+      "https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/",
+    tagId: 5
+  },
   // ---------------------- DEPTH FIRST SEARCH (6) ----------------------
   {
     name: "Flood Fill",
