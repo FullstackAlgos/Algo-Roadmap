db4dbbf8d14ec513be6e005387993718efdabb58
James Shen <jjss886@gmail.com>
2020-04-03 15:28:56 -0400

    trimming bst

--- script/seed.js
+++ script/seed.js
@@ -414,6 +414,14 @@ const questionBulk = [
     link: "https://leetcode.com/problems/convert-bst-to-greater-tree/",
     tagId: 5
   },
+  {
+    name: "Trim a Binary Search Tree",
+    description:
+      "Given a binary search tree and the lowest and highest boundaries as L and R, trim the tree so that all its elements lies in [L, R] (R >= L). You might need to change the root of the tree, so the result should return the new root of the trimmed binary search tree.",
+    difficulty: 2,
+    link: "https://leetcode.com/problems/trim-a-binary-search-tree/",
+    tagId: 5
+  },
   // ---------------------- DEPTH FIRST SEARCH (6) ----------------------
   {
     name: "Flood Fill",
