aead3bc365e2850c7fd9a5c567b43a5b03c6d462
James Shen <jjss886@gmail.com>
2020-04-03 15:25:03 -0400

    leaf similar trees added

--- script/seed.js
+++ script/seed.js
@@ -390,6 +390,14 @@ const questionBulk = [
     link: "https://leetcode.com/problems/symmetric-tree/",
     tagId: 5
   },
+  {
+    name: "Leaf-Similar Trees",
+    description:
+      "Consider all the leaves of a binary tree.  From left to right order, the values of those leaves form a leaf value sequence. Two binary trees are considered leaf-similar if their leaf value sequence is the same. Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.",
+    difficulty: 2,
+    link: "https://leetcode.com/problems/leaf-similar-trees/",
+    tagId: 5
+  },
   // ---------------------- DEPTH FIRST SEARCH (6) ----------------------
   {
     name: "Flood Fill",
