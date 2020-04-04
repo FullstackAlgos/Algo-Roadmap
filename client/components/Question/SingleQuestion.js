61f3e69d8f33dee3099ecad663905e462dd873ac
James Shen <jjss886@gmail.com>
2020-04-04 13:33:32 -0400

    removed rated difficulty from the question UI

--- client/components/Question/SingleQuestion.js
+++ client/components/Question/SingleQuestion.js
@@ -26,7 +26,7 @@ class SingleQuestion extends Component {
 
   render() {
     const { show, setActive, done, switchUserActive, q, user } = this.props,
-      { name, description, difficulty, link, ratedDifficulty, likes } = q,
+      { name, description, difficulty, link, likes } = q,
       liked = this.likedQuest(user, likes, "like"),
       disliked = this.likedQuest(user, likes, "dislike");
 
@@ -53,7 +53,8 @@ class SingleQuestion extends Component {
               </p>
 
               <p className="questionRate">
-                Difficulty: <strong className = {`difficulty${difficulty}`}>
+                Difficulty:{" "}
+                <strong className={`difficulty${difficulty}`}>
                   {difficultMap[difficulty]}
                 </strong>
               </p>
@@ -64,10 +65,6 @@ class SingleQuestion extends Component {
 
               <p className={`questionRate qRate${disliked}`}>
                 Dislikes: <strong>{this.numLikes(likes, false)}</strong>
-              </p>
-
-              <p className="questionRate">
-                Rated Difficulty: <strong>{Number(ratedDifficulty)}</strong>
               </p>
             </div>
 
@@ -86,13 +83,13 @@ class SingleQuestion extends Component {
   }
 }
 
-const mapState = state => {
+const mapState = (state) => {
   return { user: state.user };
 };
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
-    switchUserActive: (qId, qName) => dispatch(switchUserActive(qId, qName))
+    switchUserActive: (qId, qName) => dispatch(switchUserActive(qId, qName)),
   };
 };
 
