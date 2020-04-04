7e19beb349a03df0cce2d3b8f9d18734611f5c66
James Shen <jjss886@gmail.com>
2020-04-04 14:25:43 -0400

    send active user prompt into props for functioning feedback loop for roadmap questions

--- client/components/Roadmap/RoadmapQuestion.js
+++ client/components/Roadmap/RoadmapQuestion.js
@@ -2,20 +2,20 @@ import React from "react";
 import { connect } from "react-redux";
 import { switchUserActive } from "../../store";
 
-const RoadmapQuestion = ({ question, questionNum }) => {
+const RoadmapQuestion = ({ question, questionNum, switchUserActive }) => {
   const { id, name, tag, link, difficulty } = question;
 
   return (
     <span className={`roadmapQuestion roadmapQuestion${questionNum}`}>
       <h3 className="roadMapQuestTag">{tag.name}</h3>
 
-      <a href={link} target="_blank" className="questionLink linkText">
-        <h3
-          className={`difficulty${difficulty}`}
-          onClick={() => switchUserActive(id, name)}
-        >
-          {name}
-        </h3>
+      <a
+        href={link}
+        target="_blank"
+        className={`roadmapQuestLink linkText difficulty${difficulty}`}
+        onClick={() => switchUserActive(id, name)}
+      >
+        {name}
       </a>
     </span>
   );
