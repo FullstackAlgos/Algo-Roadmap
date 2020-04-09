7558495f6a04f4090384aff18ca5aeef2c0cf581
James Shen <jjss886@gmail.com>
2020-04-09 10:48:23 -0400

    establishing help open and close functionality

--- client/components/Global/HelpBar.js
+++ client/components/Global/HelpBar.js
@@ -1,9 +1,33 @@
 import React from "react";
 
-const HelpBar = () => (
-  <div className="helpBarFullDiv">
-    <h3></h3>
-  </div>
-);
+const HelpBar = () => {
+  const open = () => {
+    document.getElementById("helpBarInsideDiv").style.width = "30%";
+  };
+
+  const close = () => {
+    document.getElementById("helpBarInsideDiv").style.width = "0%";
+  };
+
+  return (
+    <div className="helpBarFullDiv">
+      <input
+        className="helpMainBtn"
+        onClick={open}
+        type="image"
+        src="/images/liveSupportIcon.png"
+        alt="Help Button"
+      />
+
+      <div className="helpBarInsideDiv">
+        <h3>Help Bar!</h3>
+
+        <a className="helpCloseBtn" onClick={close}>
+          x
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default HelpBar;
