6f5674fe826a530df7614390c83f59248cb6bf9f
James Shen <jjss886@gmail.com>
2020-04-09 23:13:07 -0400

    adjusted seed data to better protect users

--- script/seed.js
+++ script/seed.js
@@ -7,12 +7,12 @@ const {
   Like,
   ProposeQuestion,
 } = require("../server/db/models");
-
-const userBulk = [
-  { email: "james@a.com", name: "James", isAdmin: true, password: "123" },
-  { email: "jasen@a.com", name: "Jasen", isAdmin: true, password: "123" },
-  { email: "peter@a.com", name: "Peter", password: "123" },
-];
+const {
+  userBulk,
+  likeBulk,
+  userQuestBulk,
+  propQuestBulk,
+} = require("./seedData");
 
 const questionBulk = [
   // ------------------------- ARRAY (1) ---------------------------
@@ -899,41 +899,18 @@ const tagBulk = [
   },
 ];
 
-const likeBulk = [
-  { status: "like", userId: 1, questionId: 1 },
-  { status: "dislike", userId: 1, questionId: 2 },
-  { status: "like", userId: 2, questionId: 3 },
-  { status: "like", userId: 3, questionId: 4 },
-];
-
-const userQuestBulk = [
-  { userId: 1, questionId: 1 },
-  { userId: 1, questionId: 2 },
-  { userId: 2, questionId: 3 },
-  { userId: 3, questionId: 4 },
-];
-
-const propQuestBulk = [
-  {
-    name: "Spiral Matrix",
-    description:
-      "Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.",
-    difficulty: 2,
-    link: "https://leetcode.com/problems/spiral-matrix/",
-    tagId: 1,
-    userId: 1,
-  },
-];
-
 async function seed() {
   await db.sync({ force: true });
 
-  await User.bulkCreate(userBulk);
   await Tag.bulkCreate(tagBulk);
   await Question.bulkCreate(questionBulk);
-  await UserQuestion.bulkCreate(userQuestBulk);
-  await Like.bulkCreate(likeBulk);
-  await ProposeQuestion.bulkCreate(propQuestBulk);
+
+  if (userBulk && userBulk.length) {
+    await User.bulkCreate(userBulk);
+    await UserQuestion.bulkCreate(userQuestBulk);
+    await Like.bulkCreate(likeBulk);
+    await ProposeQuestion.bulkCreate(propQuestBulk);
+  }
 
   console.log(`seeded successfully`);
 }
---
+++ script/seedData.js
@@ -0,0 +1,31 @@
+export const userBulk = [
+  { email: "james@a.com", name: "James", isAdmin: true, password: "123" },
+  { email: "jasen@a.com", name: "Jasen", isAdmin: true, password: "123" },
+  { email: "peter@a.com", name: "Peter", password: "123" },
+];
+
+export const likeBulk = [
+  { status: "like", userId: 1, questionId: 1 },
+  { status: "dislike", userId: 1, questionId: 2 },
+  { status: "like", userId: 2, questionId: 3 },
+  { status: "like", userId: 3, questionId: 4 },
+];
+
+export const userQuestBulk = [
+  { userId: 1, questionId: 1 },
+  { userId: 1, questionId: 2 },
+  { userId: 2, questionId: 3 },
+  { userId: 3, questionId: 4 },
+];
+
+export const propQuestBulk = [
+  {
+    name: "Spiral Matrix",
+    description:
+      "Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.",
+    difficulty: 2,
+    link: "https://leetcode.com/problems/spiral-matrix/",
+    tagId: 1,
+    userId: 1,
+  },
+];
