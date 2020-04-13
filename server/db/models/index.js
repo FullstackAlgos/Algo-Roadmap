96aca057ac433cce6cd7b6a4003536b8da3555d4
James Shen <jjss886@gmail.com>
2020-04-13 14:08:41 -0400

    removed user questions

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,16 +1,12 @@
 const Question = require("./Question");
 const Tag = require("./Tag");
 const User = require("./user");
-const UserQuestion = require("./userQuestion");
 const Like = require("./like");
 const ProposeQuestion = require("./proposeQuestion");
 
 Question.belongsTo(Tag);
 ProposeQuestion.belongsTo(Tag);
 ProposeQuestion.belongsTo(User);
-
-User.belongsToMany(Question, { through: UserQuestion });
-Question.belongsToMany(User, { through: UserQuestion });
 
 User.hasMany(Like);
 Question.hasMany(Like);
@@ -21,7 +17,6 @@ module.exports = {
   Question,
   Tag,
   User,
-  UserQuestion,
   Like,
   ProposeQuestion,
 };
